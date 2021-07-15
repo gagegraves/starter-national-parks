c187ca3e2badb503f40c152be3221db31ef729c3
Gage Graves <gage62@yahoo.com>
2021-07-15 12:49:36 -0700

    updating the DOM checkpoint

--- index.js
+++ index.js
@@ -23,15 +23,55 @@
 //     console.log(element);
 //   }
 
-  
-const divRatingsList = document.querySelectorAll('div .rating-display')
+// const divRatingsList = document.querySelectorAll('div .rating-display')
 
-for (let divs of divRatingsList.values()) {
-    console.log(divs)
+// for (let divs of divRatingsList.values()) {
+//     console.log(divs)
+// }
+
+// const divAreasList = document.querySelectorAll('div .area-display')
+
+// for (let i = 0; i < divAreasList.length; i++) {
+//     console.log(divAreasList[i]);
+// }
+const descriptions = document.querySelectorAll(".description-display");
+
+for (let desc of descriptions.values()) {
+  let content = desc.innerText;
+  content = content.slice(0, 250);
+  content += '<a href="#">...</a>';
+  desc.innerHTML = content;
 }
 
-const divAreasList = document.querySelectorAll('div .area-display')
+const ratings = document.querySelectorAll(".rating-display .value");
 
-for (let i = 0; i < divAreasList.length; i++) {
-    console.log(divAreasList[i]);
+for (let rating of ratings) {
+  let ratingValue = parseFloat(rating.innerText);
+  console.log(ratingValue);
 }
+
+//   for (let rating of ratings) {
+//     let ratingValue = parseFloat(rating.innerText);
+
+//     if (ratingValue > 4.7) {
+//       rating.style.fontWeight = "bold";
+//       rating.style.color = "#3ba17c";
+//     }
+//   }
+
+for (let rating of ratings) {
+  let ratingValue = parseFloat(rating.innerText);
+
+  if (ratingValue > 4.7) {
+    rating.classList.add("high-rating");
+    rating.classList.remove("value");
+  }
+}
+
+const parks = document.querySelectorAll(".park-display");
+const numberParks = parks.length;
+const newElement = document.createElement("div");
+newElement.innerText = `${numberParks} exciting parks to visit`;
+newElement.classList.add("header-statement");
+const header = document.querySelector("header");
+header.appendChild(newElement);
